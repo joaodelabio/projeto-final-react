676a8fb623cf0ee387e4adbfbf8ede7cb84451b1
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-04 22:07:44 -0300

    organizando essas bostinha

---
+++ src/views/Galeria/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header";
+import GridGaleria from "../../components/grid-galeria";
+import CardImagens from "../../components/card-imagens";
+
+export default function Galeria() {
+    return (
+        <>
+        <Header/>
+        <GridGaleria/>
+        <CardImagens/>
+        </>
+    );
+}
---
+++ src/views/Main/index.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header"
+import Fogo from "../../components/fogo";
+
+export default function MainPage() {
+    return (
+        <>
+        <Header/>
+        <Fogo/>
+        </>
+    );
+}
