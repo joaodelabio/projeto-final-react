131aa6ffe59ab6f67e0a4f1667c1058855b5f0d4
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-04 22:08:03 -0300

    teste

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,12 @@
-import Header from "../components/Header"
+import Main from "../views/Main"
+import Galeria from "../views/Galeria"
 
 function App() {
 
   return (
     <>
-    <Header/>
+    {/*<Main/>*/}
+    <Galeria/>
     </>
   )
 }
