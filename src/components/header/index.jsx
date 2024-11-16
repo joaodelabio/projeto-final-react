2d1e862917d4305a65b1d5ae5a42ae272eab6a6e
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-16 15:54:28 -0300

    vou deixar isso mais bonito, eu prometo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,11 +5,11 @@ export default function Header() {
         <>
         <header>
             <div className='logo'>
-                <h1>Loja sabão macaco</h1>
+                <h1>Sabão Macaco</h1>
             </div>
             <nav className='header-links'>
                 <p className='header-links'>Home</p>
-                <p className='header-links'>Loja</p>
+                <p className='header-links'>Produtos</p>
                 <p className='header-links'>Carrinho</p>
                 <p className='header-links'>Contatos</p>
             </nav>
