f270e74ecd0f39ee9457a4f6be21c99e47043ccc
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-16 15:55:45 -0300

    rotas funfando

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from "../views/Main"
 import Loja from "../views/Loja"
 
 function App() {
 
   return (
-    <>
-    {/*<Main/>*/}
-    <Loja/>
-    </>
-  )
+    <Router>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/grid-loja" element={<Loja />} />
+        </Routes>
+    </Router>
+);
 }
-
 export default App
