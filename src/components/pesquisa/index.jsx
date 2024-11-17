3bcdca740497b93274a6a447456777ef97c2b633
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-17 12:39:49 -0300

    1 unidade de mudanca

--- src/components/pesquisa/index.jsx
+++ src/components/pesquisa/index.jsx
@@ -3,7 +3,7 @@ import './pesquisa.css';
 export default function Pesquisa({ onPesquisaChange }) {
     return (
         <div className='container-pesquisa'>
-            <input type="text" placeholder='Procurar por imagem' onChange={(e) => onPesquisaChange(e.target.value)}/>
+            <input type="text" placeholder='Pesquisar produto' onChange={(e) => onPesquisaChange(e.target.value)}/>
         </div>
     );
 }
