7caaeed684151a9ed2fa63cb3310b7fa50bc1987
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-17 12:09:17 -0300

    rota pro carrinho

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from "../views/Main"
 import Loja from "../views/Loja"
+import Carrinho from '../views/Carrinho';
 
 function App() {
 
   return (
     <Router>
         <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/grid-loja" element={<Loja />} />
+            <Route path="/" element={<Main/>}/>
+            <Route path="/Loja" element={<Loja/>}/>
+            <Route path="/Carrinho" element={<Carrinho/>}/>
         </Routes>
     </Router>
 );
