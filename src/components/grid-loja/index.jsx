9f316c373fe3f0a0005069161f0e4ed6f2a481e1
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-20 15:05:02 -0300

    big update pra caixa de alerta ficar sexy

--- src/components/grid-loja/index.jsx
+++ src/components/grid-loja/index.jsx
@@ -1,33 +1,34 @@
 import './loja.css';
 import CardImagens from '../card-imagens';
 import Pesquisa from '../pesquisa';
+import Alerta from '../alerta';
 import { useState } from 'react';
 
 export default function GridLoja() {
     const [termoPesquisa, setTermoPesquisa] = useState('');
     const [carrinho, setCarrinho] = useState([]);
+    const [alerta, setAlerta] = useState(null);
 
     const handlePesquisaChange = (termo) => {
         setTermoPesquisa(termo.toLowerCase());
     };
 
     const produtos = [
-        { id: 1, src: './src/assets/imagens/sabao1.png', title: "teste1", description: "texto texto texto" },
-        { id: 2, src: './src/assets/imagens/sabao2.png', title: "teste2", description: "texto texto texto" },
-        { id: 3, src: './src/assets/imagens/sabao3.png', title: "teste3", description: "texto texto texto" },
-        { id: 4, src: './src/assets/imagens/sabao4.png', title: "teste4", description: "texto texto texto" },
-        { id: 5, src: './src/assets/imagens/sabao5.png', title: "teste5", description: "texto texto texto" },
-        { id: 6, src: './src/assets/imagens/sabao6.png', title: "teste6", description: "texto texto texto" },
+        { id: 1, src: './src/assets/imagens/sabao1.png', title: "Sabão 1", description: "$ 50,00" },
+        { id: 2, src: './src/assets/imagens/sabao2.png', title: "Sabão 2", description: "$ 10.000,00" },
+        { id: 3, src: './src/assets/imagens/sabao3.png', title: "Sabão 3", description: "$ 750,00" },
+        { id: 4, src: './src/assets/imagens/sabao4.png', title: "Sabão 4", description: "$ 69,69" },
+        { id: 5, src: './src/assets/imagens/sabao5.png', title: "Sabão 5", description: "$ 10,00" },
+        { id: 6, src: './src/assets/imagens/sabao6.png', title: "Sabão 6", description: "$ 0,00" },
     ];
 
     const produtosFiltrados = produtos.filter((produto) =>
-        produto.title.toLowerCase().includes(termoPesquisa) ||
-        produto.description.toLowerCase().includes(termoPesquisa)
+        produto.title.toLowerCase().includes(termoPesquisa) // || produto.description.toLowerCase().includes(termoPesquisa)
     );
 
     const addCarrinho = (produto) => {
         setCarrinho([...carrinho, produto]);
-        alert(`"${produto.title}" adicionado ao carrinho.`);
+        setAlerta(`${produto.title} adicionado ao carrinho`);
     };
 
     return (
@@ -44,6 +45,7 @@ export default function GridLoja() {
                     />
                 ))}
             </div>
+            {alerta && <Alerta mensagem={alerta} onClose={() => setAlerta(null)} />}
         </section>
     );
 }
